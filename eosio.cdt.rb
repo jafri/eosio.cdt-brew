da2f14ab44197ce06adb131b6b8bfbdded9a6c9f
jafri <syedjafri97@gmail.com>
2022-01-31 12:21:41 -0700

    sha

--- eosio.cdt.rb
+++ eosio.cdt.rb
@@ -25,6 +25,7 @@ class EosioCdt < Formula
    bottle do
       root_url "https://github.com/eosio/eosio.cdt/releases/download/v1.7.0"
       sha256 mojave: "cd4183525582edef621eb59c5ff485f48ecf5d28a37344f65a556ec965ba8109"
+      sha256 catalina: "b6d91fc70d15a6c2c2f0c29ac0436421272cefb50f9d1362afe8057aa08174b9"
    end
    def install
       raise "Error, only supporting binary packages at this time"
